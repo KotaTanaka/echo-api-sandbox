9a209f901513a761e1a58da0acda948a2928068d
Kota Tanaka <te11k.tanaka@gmail.com>
2019-10-11 16:41:47 +0900

    feat: 初回のstart-dockerではDBが作られるのを待つ

--- start-docker.sh
+++ start-docker.sh
@@ -2,4 +2,12 @@
 # ------------------------------
 # コンテナを起動するスクリプト
 # ------------------------------
+DBPATH=./docker/db/mysql_data/find_wifi_db
+
+# コンテナの起動
 docker-compose up -d
+
+# 初回の場合DBが作られるのを待つ
+if [ -d $DBPATH ]; then exit 0
+else /bin/echo -n "Initialize database ... "; fi
+while [ ! -d $DBPATH ]; do sleep 1; done; echo "done!"
