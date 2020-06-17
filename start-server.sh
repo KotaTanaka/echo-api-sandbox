5814860925b6f2cfe27f42d733f2fa2b26d735e9
KotaTanaka <te11k.tanaka@gmail.com>
2020-06-17 20:21:14 +0900

    setup: DBが作られるのを待つサーバー起動スクリプト

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 # ------------------------------
 # サーバーを起動するスクリプト
 # ------------------------------
-docker-compose exec app sh -c "go run app/main.go"
+DBPATH=./docker/db/mysql_data/find_wifi_db
+RUN="docker-compose exec app sh -c \"go run app/main.go\""
+
+echo "========================="
+echo "  Find Wi-Fi API Server"
+echo "========================="
+
+# 初回の場合DBが作られるのを待つ
+if [ -d $DBPATH ]; then
+  eval $RUN
+  exit 0
+else
+  /bin/echo -n "Initialize database ... "
+fi
+while [ ! -d $DBPATH ]; do
+  sleep 1
+done
+echo "done."
+eval $RUN
