4a662a9812eb0047e8bde0f01413bed66b9161f8
KotaTanaka <te11k.tanaka@gmail.com>
2020-07-02 20:39:19 +0900

    fix: エリアマスタにGormのプロパティが含まれていた

--- data/client/area.go
+++ data/client/area.go
@@ -3,14 +3,11 @@ Package clientdata クライアントAPI関連構造体
 */
 package clientdata
 
-import "github.com/jinzhu/gorm"
-
 /*
 AreaMasterResponseElement | エリアマスタ取得レスポンス要素
 @type Response
 */
 type AreaMasterResponseElement struct {
-	gorm.Model
 	AreaKey   string `json:"areaKey"`
 	AreaName  string `json:"areaName"`
 	ShopCount int    `json:"shopCount"`
