de4f8dfca41f5701606b2c57d2bdb5a5796257cf
KotaTanaka <te11k.tanaka@gmail.com>
2022-06-22 01:26:10 +0900

    feat: Dockerfileからの卒業

--- redoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# ------------------------------
-# openapi.yml からAPI仕様書を生成するスクリプト
-# ------------------------------
-docker-compose exec app sh -c "redoc-cli bundle app/openapi.yml -o app/redoc.html"
-
-if [ $? = 0 ]; then
-  echo "\n[API仕様書] http://localhost:1323/doc"
-fi
